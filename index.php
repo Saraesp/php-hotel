777a602cea41871bd79dcea00dc88c61bea04272
Saraesp <saraesposito145@gmail.com>
2023-01-30 21:21:29 +0100

    impostazione stile tabella

--- index.php
+++ index.php
@@ -49,36 +49,44 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65"crossorigin="anonymous">
-    <link href="https://fonts.googleapis.com/css2?family=Bodoni+Moda:opsz,wght@6..96,700&family=Montserrat:wght@200;300;400&family=PT+Sans&family=Roboto:wght@400;500&family=Sofia+Sans+Condensed:wght@600&display=swap" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    
+    <link href="https://fonts.googleapis.com/css2?family=Bodoni+Moda:opsz,wght@6..96,700&family=Montserrat:wght@200;300;400&family=PT+Sans&family=Roboto:wght@600;800&family=Sofia+Sans+Condensed:wght@600&display=swap" rel="stylesheet">
 
     <link rel="stylesheet" href="./css/style.css">
 </head>
 <body>
-    <div>
-        <div>
-            <h1>HOTEL TABLE</h1>
+    <div class="container-sm mt-4">
+        
+        <div class="bg-grey">
+            <h1 class="border-start border-black text-white">HOTEL TABLE</h1>
         </div>
-        <table class="mt-3">
+
+        <div class="d-flex pe-2">
+        <input class="form-control form-control-sm form-style" type="text">
+        <button type="button" class="btn button">Cerca</button>
+        </div>
+
+        <table class="mt-4 w-100 table-hover">
             <thead>
-                <th>NAME</th>
-                <th>DESCRIPTION</th>
-                <th>PARKING</th>
-                <th>VOTE</th>
-                <th>DISTANCE TO CENTER</th>
+                <th class="p-3 text-center style-text">NAME</th>
+                <th class="p-3 text-center style-text">DESCRIPTION</th>
+                <th class="p-3 text-center style-text">PARKING</th>
+                <th class="p-3 text-center style-text">VOTE</th>
+                <th class="p-3 text-center style-text">DISTANCE TO CENTER</th>
             </thead>
-            <tbody>
+            <tbody class="">
                 <?php foreach($hotels as $hotel){ ?>
-                    <tr>
-                        <td><?php echo $hotel['name']; ?></td>
-                        <td><?php echo $hotel['description']; ?></td>
-                        <td><?php if($hotel['parking']){
+                    <tr class="bg-light-blue">
+                        <td class="p-2 table-blue"><?php echo $hotel['name']; ?></td>
+                        <td class="p-2 text-center"><?php echo $hotel['description']; ?></td>
+                        <td class="text-center p-2"><?php if($hotel['parking']){
                                 echo 'Sì';
                             }else{
                                 echo 'No';
                             };?></td>
-                        <td><?php echo $hotel['vote']; ?></td>
-                        <td><?php echo $hotel['distance_to_center']; ?></td>
+                        <td class="text-center p-2"><?php echo $hotel['vote']; ?></td>
+                        <td class="text-center p-2"><?php echo $hotel['distance_to_center']; ?></td>
                     </tr>
                 <?php } ?>
             </tbody>
